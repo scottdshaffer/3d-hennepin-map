495d97eece8bb7bffbe8696e2b1854827dc02616
scottdshaffer <47195231+scottdshaffer@users.noreply.github.com>
2022-06-26 20:30:32 -0500

    Add files via upload

---
+++ 3d_henn.R
@@ -0,0 +1,44 @@
+#3d population density map
+
+#load packages
+library(tidyverse)
+library(sf)
+library(units)
+library(rayshader)
+library(tidycensus)
+
+henn_pop_dens <- get_acs(geography = "tract",
+                         state = "MN",
+                         county = "Hennepin",
+                         variable = "B01003_001",
+                         year = 2020,
+                         geometry = TRUE) %>% 
+  mutate(area = st_area(geometry))
+
+henn_pop_dens$area <- set_units(henn_pop_dens$area, mi^2)
+henn_pop_dens$dens <- henn_pop_dens$estimate/henn_pop_dens$area
+henn_pop_dens
+
+showtext_opts(dpi = 96)
+pop_dens_map <- ggplot(henn_pop_dens) + geom_sf(aes(fill = as.numeric(dens)),
+                                                color = NA) +
+  labs(title = "Hennepin County, Minnesota",
+       caption = "Scott Shaffer | April 2022 | tidycensus and rayshader packages") +
+  scale_fill_viridis_c(name = "Population per\nsquare mile",
+                       labels = scales::label_comma(scale = 1/1000, suffix = "k"),
+                       direction = -1,
+                       option = "mako") +
+  coord_sf(datum = NA) +
+  theme_minimal() + theme(text = element_text(family = "crimson"),
+                          plot.title = element_text(hjust = .5),
+                          plot.caption = element_text(face = "italic",
+                                                      hjust = .5),
+                          plot.background = element_rect(color = NA, fill = "white"),
+                          legend.position = "bottom")
+showtext_opts(dpi = 300)
+
+plot_gg(pop_dens_map,multicore = TRUE,width=5,height=5,scale=200,windowsize=c(1600,1200),
+        zoom = 0.6, theta = 30, phi = 40)
+render_camera(zoom = 0.45, theta = 30, phi = 35)
+render_snapshot()
+
